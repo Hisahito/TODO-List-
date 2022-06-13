81dff6ef39658a48c7aeb94db78eca049cf4bb29
hisahito <hisahito706@gmail.com>
2022-06-13 18:16:44 -0500

    Adding two more methods in todo-list.class.js file to store data in local storage

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -4,18 +4,19 @@ export class TodoList {
 
     constructor() {
 
-        this.todos = [];
-
+        //this.todos = [];
+        this.cargarLocalStorage();
     }
 
     nuevoTodo( todo ){
         this.todos.push( todo );
+        this.guardarLocalStorage();
     }
 
     eliminarTodo( id ) {
 
         this.todos = this.todos.filter( todo => todo.id != id )
-
+        this.guardarLocalStorage();
     }
 
     marcarCompletado( id ) {
@@ -23,6 +24,7 @@ export class TodoList {
         for( const todo of this.todos ) {
             if( todo.id == id ) {
                 todo.completado = !todo.completado;
+                this.guardarLocalStorage();
                 break;
             }
         }
@@ -33,6 +35,20 @@ export class TodoList {
     eliminarCompletados() {
         
         this.todos = this.todos.filter( todo => !todo.completado )
+        this.guardarLocalStorage();
+    }
+
+    guardarLocalStorage() {
+
+        localStorage.setItem( 'todo', JSON.stringify( this.todos) );
+
+    }
+
+    cargarLocalStorage() {
+
+       this.todos = ( localStorage.getItem('todo') )
+                    ? JSON.parse( localStorage.getItem('todo') )
+                    : [];
 
     }
 }
