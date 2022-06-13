d62e7909875b95230966b6f82817cb9c3a9cffd5
hisahito <hisahito706@gmail.com>
2022-06-12 23:59:08 -0500

    Changing the componentes.js file with the logic to add and render the todo in each html list first child

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,19 +1,27 @@
-import '../css/componentes.css';
-//import webpacklogo from '../assets/img/webpack-logo.png';
 
-export const saludar = ( nombre ) => {
+// referencias en el HTML
+const divTodoList = document.querySelector('.todo-list');
 
-    console.log("creando etiqueta h1")
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${ nombre } !!!`;
+export const crearTodoHtml = ( todo ) => {
 
-    document.body.append( h1 )
+    const htmlTodo = `
+    <li class="${ (todo.completado) ? 'completed' : ''  }" data-id="${ todo.id }">
+        <div class="view">
+            <input class="toggle" type="checkbox" ${ (todo.completado) ? 'checked' : ''  }>
+            <label>${ todo.tarea }</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+</li>`;
 
-    //img
-    console.log( webpacklogo );
-    const img = document.createElement('img');
-    img.src = webpacklogo;
-    document.body.append( img );
+    const div = document.createElement('div');
+    div.innerHTML = htmlTodo;
+
+    divTodoList.append( div.firstElementChild );
+
+    return div;
 
 }
+
+
